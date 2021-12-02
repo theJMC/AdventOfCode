1aeb70134cf46b5a5b99fb8c820caf6966ead3f7
James <james@jamesmcc.co.uk>
2021-12-02 18:18:42 +0000

    Day 2

---
+++ Day2/day2P2.py
@@ -0,0 +1,29 @@
+
+
+with open("input.txt", "r") as file:
+    temp = file.readlines()
+    lines = []
+    for item in temp:
+        lines.append(item.rstrip().split(" "))
+
+
+    pos = 0
+    dep = 0
+    aim = 0
+
+    for item in lines:
+        match item[0]:
+            case "forward":
+                pos += int(item[1])
+                dep += aim * int(item[1])
+            case "down":
+                aim += int(item[1])
+            case "up":
+                aim -= int(item[1])
+            case _:
+                print(f"ERROR {item[0]}")
+
+
+    print(f"Position: {pos}")
+    print(f"Depth: {dep}")
+    print(f"Ans: {pos * dep}")

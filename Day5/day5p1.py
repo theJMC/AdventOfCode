5f0e160e6e8f4bae95e7ee334bd83fc403893554
James <james@jamesmcc.co.uk>
2021-12-06 14:33:10 +0000

    Completed D5P1

--- Day5/day5p1.py
+++ Day5/day5p1.py
@@ -1,5 +1,5 @@
-DIMENSIONS = 9
-FILE = 'Day5/testinput.txt'
+DIMENSIONS = 1000
+FILE = 'Day5/input.txt'
 
 def createGraph():
     graph = [[0 for _ in range(DIMENSIONS)] for _ in range(DIMENSIONS)]
@@ -10,7 +10,9 @@ def getLines():
     with open(FILE) as f:
         for line in f:
             splitLine = line.rstrip().split(" -> ")
-            bothSplit = (splitLine[0].split(","), splitLine[1].split(","))
+            split1 = [int(item) for item in splitLine[0].split(",")]
+            split2 = [int(item) for item in splitLine[1].split(",")]
+            bothSplit = (split1, split2)
             lines.append(bothSplit)
     return lines
 
@@ -31,25 +33,33 @@ def populateGraph(lines, graph):
     points = []
 
     for index, line in enumerate(acceptableLines):
-        if x_equal(line):
-            x = line[0][0]
-            for y in range(int(line[0][1]), int(line[1][1])):
+        print(f"working on line: {line}")
+        x1 = line[0][0]
+        x2 = line[1][0]
+        y1 = line[0][1]
+        y2 = line[1][1]
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                print(f"appended point: {(x, y)}")
                 points.append((x, y))
-        elif y_equal(line):
-            y = line[0][1]
-            for x in range(int(line[0][0]), int(line[1][0])):
-                points.append((x, y))
+
+    for item in points:
+        graph[item[1]][item[0]] += 1
     
     return graph, points
 
 if __name__ == "__main__":
     graph = createGraph()
-    print(graph)
     lines = getLines()
     graph, points = populateGraph(lines, graph)
-    
-    for item in points:
-        print(points)
 
-    for i in range(len(graph)-1, 0, -1):
-        print(graph[i])
+    numOfgreaterThanOne = 0
+
+    for i in range(len(graph)):
+        #print(graph[i])
+        for j in range(len(graph[i])):
+            if graph[i][j] > 1:
+                numOfgreaterThanOne += 1
+
+    print(f"score: {numOfgreaterThanOne}")
+

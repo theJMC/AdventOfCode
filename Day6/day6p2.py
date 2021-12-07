483abd36f99c147616aacbfc4b7286f5ee704895
James <james@jamesmcc.co.uk>
2021-12-07 11:36:31 +0000

    completed day6p2

--- Day6/day6p2.py
+++ Day6/day6p2.py
@@ -1,84 +1,51 @@
-TEST = True
-DAYS = 3
+TEST = False
+DAYS = 256
 
-
-class Fish:
-    def __init__(self, daysUntil=9):
-        self.daysUntil = daysUntil
-
-    def __repr__(self):
-        return str(self.daysUntil)
-
-    def increment(self):
-        if self.daysUntil == 0:
-            self.daysUntil = 6
-            return True
-        self.daysUntil -= 1
-
-
+validTimers = [x for x in range(0, 9)]
 
 def getData():
     if TEST:
         return [3,4,3,1,2]
     else:
         with open('Day6/input.txt', 'r') as f:
-            return f.readline().rstrip().split(",")
-"""
-def iterate(possibilities):
-    temp = possibilities[0]
-    for i in range(len(possibilities)-1):
-        possibilities[i] = possibilities[i+1]
-    if temp == 0:
-        possibilities[-1] = temp
-    else:
-        possibilities[6] += temp
-        possibilities[8] += temp
-    return possibilities
+            list = f.readline().rstrip().split(",")
+            return [int(x) for x in list]
+
+
+def sim(fishDict):
+    for day in range(DAYS):
+        print(f"Day {day}")
+        spawnReset = fishDict[0]
+        for timer in validTimers:
+            match timer:
+                case 8:
+                    fishDict[timer] = spawnReset
+                case 6:
+                    fishDict[timer] = fishDict[timer + 1] + spawnReset
+                case _:
+                    fishDict[timer] = fishDict[timer + 1]
+    return fishDict
 
 if __name__ == "__main__":
     data = getData()
-    possibilities = [0 for _ in range(9)]
-    print(possibilities)
-    for i in range(len(data)):
-        possibilities[data[i]] += 1
-
-    for i in range(DAYS):
-        possibilities = iterate(possibilities)
-
-    print(possibilities)
-    print(sum(possibilities))"""
-
-def nicePrint(dict):
-    for key in dict:
-        print(f"{key} : {dict[key]}")
-
-if __name__ == "__main__":
-    data = getData()
-    numbersDict = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0
+    fishDict = {
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+        7:0,
+        8:0
     }
 
-    for i in range(len(data)):
-        numbersDict[data[i]] += 1
+    # Get data into dict
+    for item in data:
+        fishDict[item] += 1
 
-    for i in range(DAYS):
-        for key in numbersDict:
-            if numbersDict[0] == 0:
-                numbersDict[-1] = 0
-            else:
-                temp = numbersDict[0]
-            
-                
-
-    nicePrint(numbersDict)
+    fishDict = sim(fishDict)
+    print(sum(fishDict.values()))
 
 
 
